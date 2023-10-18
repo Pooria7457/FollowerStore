d6c45718d747f154634f61072914d2fc5862f65d
Pooria 7457 <pooria.eb7457@gmail.com>
2023-10-19 01:49:23 +0330

    First Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.ebrahimipooria.followerstore"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.ebrahimipooria.followerstore"
